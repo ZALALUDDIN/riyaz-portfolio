9e09883b1eed5be1bb3e3bbbfc4ef533c4ff73cf
ZALALUDDIN <masumctg8@gmail.com>
2023-05-08 00:41:27 +0600

    first commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Shah Md. Nesar Uddin Riyaz</span> Research and brainstorm. Think about my background, education, awards, skills and other attributes that my developed or earned throughout my career or education.... <br>This is the introduction....<br>Share relevant skills.... <br>Explain the career goals."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
